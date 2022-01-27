8cc27d06b5a899a6704328299a58542f06cacf28
v-jonathangu <v-jonathangu@microsoft.com>
2022-01-27 10:57:18 -0600

    ios build script

---
+++ ios/appcenter-post-clone.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+#Place this script in project/android/app/
+
+cd ..
+
+# fail if any command fails
+set -e
+# debug log
+set -x
+
+cd ..
+git clone -b beta https://github.com/flutter/flutter.git
+export PATH=`pwd`/flutter/bin:$PATH
+
+flutter channel stable
+yes | flutter doctor --android-licenses
+flutter doctor
+
+echo "Installed flutter to `pwd`/flutter"
+
+# build APK
+# if you get "Execution failed for task ':app:lintVitalRelease'." error, uncomment next two lines
+# flutter build apk --debug
+# flutter build apk --profile
+flutter build ios --release --no-codesign --build-number $APPCENTER_BUILD_ID
+
